6522e5427ca593004270d9e7b32cced61c982851
eliasz.robakiewicz <eliasz.robakiewicz@stermedia.eu>
2023-11-19 15:04:20 +0100

    finished project, added struct and gorutines

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"strings"
+	"sync"
 
 	// import helper package from helper folder we need to specify the path to the package
 	"booking_app/helper"
+	"time"
 )
 
 var conferenceName string = "2023 Go Conference"
@@ -16,48 +18,73 @@ const conferenceTickets int = 50
 // to make function available to be exported it has to start with capital letter
 var RemainingTickets int = 50
 
-// Slice in Go is dynamic array, it can grow and shrink
-var ListOfBookings = []string{} // empty slice of strings
+// Slice in Go is dynamic array, it can grow and shrink, to init slice of maps we need to use 'make' function
+var ListOfBookings = make([]UserData, 0) // empty slice of strings
 
 // array definition has to have a fixed size [50] and type of data in array e.g. [10]string{} that definition is also its type
 // var ListOfBookings = [50]string{} // empty array of 50 strings
 // var bookings [50]string // empty array of 50 strings
 
+// struct is a data structure that can hold different types of data, it is similar to clas in Python or Java
+// to init struct we need to use 'type' keyword
+type UserData struct {
+	firstName       string
+	lastName        string
+	email           string
+	numberOfTickets int
+}
+
+// If main treat is finished it will not wait for other threads to finish, to wait for other threads to finish we need to use sync package
+var wg = sync.WaitGroup{}
+
 func main() {
 
 	greetUsers()
 
-	for {
+	userFirstName, userLastName, userEmail, numberOfTickets := getUserInput()
+	isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(userFirstName, userLastName, userEmail, numberOfTickets, RemainingTickets)
 
-		userFirstName, userLastName, userEmail, numberOfTickets := getUserInput()
-		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(userFirstName, userLastName, userEmail, numberOfTickets, RemainingTickets)
+	if isValidName && isValidEmail && isValidTicketNumber {
 
-		if isValidName && isValidEmail && isValidTicketNumber {
+		// map is a key value pair data structure, to init map we need to use 'make' function
+		var userData = UserData{
+			firstName:       userFirstName,
+			lastName:        userLastName,
+			email:           userEmail,
+			numberOfTickets: numberOfTickets,
+		}
+		//add to map
+		// userData["firstName"] = userFirstName
+		// userData["lastName"] = userLastName
+		// userData["email"] = userEmail
+		// userData["numberOfTickets"] = strconv.FormatInt(int64(numberOfTickets), 10)
 
-			bookTicket(numberOfTickets, userFirstName, userLastName)
+		bookTicket(numberOfTickets, userFirstName, userLastName, userData)
+		// use keyword 'go' to run function in a separate thread and add it to wait group
+		wg.Add(1)
+		go sendTicekt(userFirstName, userLastName, numberOfTickets, conferenceName)
 
-			firstNames := getFirstNames()
-			fmt.Printf("People registered are: %v\n", strings.Join(firstNames, ", "))
+		firstNames := getFirstNames()
+		fmt.Printf("People registered are: %v\n", strings.Join(firstNames, ", "))
 
-			var noTicketsRemaining bool = RemainingTickets == 0
-			if noTicketsRemaining {
-				fmt.Println("The conference is now fully booked.")
-				break
-			}
+		var noTicketsRemaining bool = RemainingTickets == 0
+		if noTicketsRemaining {
+			fmt.Println("The conference is now fully booked.")
+		}
 
-		} else {
-			if !isValidName {
-				fmt.Println("Please enter a valid name.")
-			}
-			if !isValidEmail {
-				fmt.Println("Please enter a valid email.")
-			}
-			if !isValidTicketNumber {
-				fmt.Printf("Please enter a valid number of tickets. There are only %v tickets remaining.\n", RemainingTickets)
-			}
+	} else {
+		if !isValidName {
+			fmt.Println("Please enter a valid name.")
+		}
+		if !isValidEmail {
+			fmt.Println("Please enter a valid email.")
+		}
+		if !isValidTicketNumber {
+			fmt.Printf("Please enter a valid number of tickets. There are only %v tickets remaining.\n", RemainingTickets)
 		}
 	}
-
+	// wait for all threads to finish
+	wg.Wait()
 }
 
 func greetUsers() {
@@ -71,9 +98,7 @@ func getFirstNames() []string {
 	firstNames := []string{}
 
 	for _, booking := range ListOfBookings {
-		// slice string on ' ' and return array of strings
-		var names = strings.Fields(booking)
-		firstNames = append(firstNames, names[0])
+		firstNames = append(firstNames, booking.firstName)
 	}
 	return firstNames
 }
@@ -102,11 +127,19 @@ func getUserInput() (string, string, string, int) {
 	return userFirstName, userLastName, userEmail, numberOfTickets
 }
 
-func bookTicket(numberOfTickets int, userFirstName string, userLastName string) {
+func bookTicket(numberOfTickets int, userFirstName string, userLastName string, userData UserData) {
 	// save name to slice
 	RemainingTickets = RemainingTickets - numberOfTickets
-	ListOfBookings = append(ListOfBookings, userFirstName+" "+userLastName)
+	ListOfBookings = append(ListOfBookings, userData)
 
 	fmt.Printf("There are now %v tickets remaining.\n", RemainingTickets)
 	fmt.Printf("There are %v people registered for the conference.\n", len(ListOfBookings))
+}
+
+func sendTicekt(userFirstName string, userLastName string, numberOfTickets int, conferenceName string) {
+	time.Sleep(3 * time.Second)
+	var ticket = fmt.Sprintf("Hi %v %v, thank you for booking %v tickets for %v", userFirstName, userLastName, numberOfTickets, conferenceName)
+	fmt.Printf("Sending ticket: \n %v to %v %v\n", ticket, userFirstName, userLastName)
+	// remove thread from wait group
+	wg.Done()
 }
